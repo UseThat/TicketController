d59ffbd6f2aae5064cb291be6f4b9c8861b104f9
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2020-07-21 15:08:28 +0200

    Add Class Passenger

--- src/TicketController.java
+++ src/TicketController.java
@@ -1,7 +1,8 @@
 public class TicketController {
     public static void main(String[] args) {
         Ticket ticket = new Ticket(40);
-
+        Passenger passenger = new Passenger("Mateusz","WYgoda");
+        System.out.println(passenger);
         System.out.println(ticket);
     }
 }
